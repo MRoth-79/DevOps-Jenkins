e55ac1415025490d9788e670f0265500d19620db
MRoth-79 <maorot@gmail.com>
2025-01-27 00:04:00 +0200

    employees.json 1.3.1

--- Task.py
+++ Task.py
@@ -1,15 +1,15 @@
 import json
 
 with open('employees.json' , 'r') as file:
-    Task = file.read()
-
-data = json.loads(Task)
+    data = json.load(file)
 
 #---------------------------------------------------------------------#
-##Task 1 : Print the name of the company.
+
+#Task 1 : Print the name of the company.
 print('\n1) Company Name:', data['company'])
 
 #---------------------------------------------------------------------#
+
 #Task 2: Print the name of each employee along with the total number of hours they have spent across all projects.
 print('\n2) People\'s names - hours spent on projects: ')
 for employee in data['employees']:
@@ -17,7 +17,8 @@ for employee in data['employees']:
     print(f"{employee['name']} -> Total hours spent on projects: {total_hours} h")
 
 #---------------------------------------------------------------------#
-#Task 3: Print the department with the highest total project hours.
+
+Task 3: Print the department with the highest total project hours.
 department_hours = {}
 for employee in data['employees']:
     total_hours = sum(project['hours_spent'] for project in employee['projects'])
@@ -32,6 +33,7 @@ highest_department = max(department_hours, key=department_hours.get)
 print('\n3) The Department with Highest Total Project Hours: \n ', highest_department)
 
 #---------------------------------------------------------------------#
+
 # Task 4: Print the names of all employees who have "Python" as a skill
 print('\n4) Employees with Python Skill: ')
 python_skill_employees = [
@@ -40,6 +42,7 @@ python_skill_employees = [
 print(' , '.join(python_skill_employees))
 
 #---------------------------------------------------------------------#
+
 # Task 5: Print the names of employees who worked on projects with more than 100 hours spent
 print('\n5) Employees who Worked on Projects with More than 100 Hours Spent: ')
 employees_more_than_100_hours = []
